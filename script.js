095fefc1c8db2a1032aa72e17d99167a6c45df9d
Alicia <130313978+tanningexpert@users.noreply.github.com>
2023-06-28 17:09:14 -0500

    Added JS to order button display

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
+
+window.addEventListener("DOMContentLoaded", (event) => {
+    
 //DISPLAY ORDER NOW BUTTON WHEN USER HOVERS OVER DIV
 //CREATE VARIABLE AND LINK TO DOM ELEMENT
-const orderBtn = document.getElementsByClassName("orderBtn");
-const gridDiv = document.getElementsByClassName("imageWrapper");
-console.log(orderBtn);
-//ADD EVENT LISTENER FOR HOVER; document.addEventListener("click", myFunction);
+const orderBtns = document.getElementsByClassName("component-cta-btn");
+const gridDivs = document.getElementsByClassName("pokeyourway-item");
+console.log(orderBtns);
+console.log("gridDivs", gridDivs);
 
-gridDiv[0].addEventListener("mouseover", () => {
-    orderBtn.style.display = "block";
-});
+//FOR EACH ELEMENT ADD EVENT LISTENER FOR HOVER; 
+//array1.forEach(element => console.log(element));
+//document.addEventListener("click", myFunction);
+console.log("gridDivs.length", gridDivs.length)
+for (let i = 0; i < gridDivs.length; i++) {
+    //console.log(i)
+    //console.log("gridDivs[i]", gridDivs[i])
+
+    gridDivs[i].addEventListener("mouseover",() =>{
+       orderBtns[i].style.display = "block";
+    })
+    gridDivs[i].addEventListener("mouseout",() =>{
+        orderBtns[i].style.display = "none";
+     })
+};
+
+
+//gridDiv[0].addEventListener("mouseover", () => {
+//});
 //CHANGE ELEMENT DISPLAY TO VISIBLE 
+
+
+});
