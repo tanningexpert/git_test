cabeb05096e3afba7561bb01216cbde50fb43924
Alicia <130313978+tanningexpert@users.noreply.github.com>
2023-06-30 18:10:18 -0500

    Added hover animations and styled order grid

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 
-window.addEventListener("DOMContentLoaded", (event) => {
+////window.addEventListener("DOMContentLoaded", (event) => {
     
 //DISPLAY ORDER NOW BUTTON WHEN USER HOVERS OVER DIV
 //CREATE VARIABLE AND LINK TO DOM ELEMENT
 const orderBtns = document.getElementsByClassName("component-cta-btn");
 const gridDivs = document.getElementsByClassName("pokeyourway-item");
+const menuImages = document.getElementsByClassName("pokeyourway-item-image")
 console.log(orderBtns);
 console.log("gridDivs", gridDivs);
+console.log(menuImages)
 
 //FOR EACH ELEMENT ADD EVENT LISTENER FOR HOVER; 
 //array1.forEach(element => console.log(element));
@@ -17,12 +19,19 @@ for (let i = 0; i < gridDivs.length; i++) {
     //console.log("gridDivs[i]", gridDivs[i])
 
     gridDivs[i].addEventListener("mouseover",() =>{
-       orderBtns[i].style.display = "block";
+       orderBtns[i].style.display = "inline-block";
+       menuImages[i].style.width = "110%"
+       menuImages[i].style.height = "auto"
+
     })
     gridDivs[i].addEventListener("mouseout",() =>{
         orderBtns[i].style.display = "none";
+        menuImages[i].style.width = "90%"
+        menuImages[i].style.height = "auto"
+
      })
 };
+
 
 
 //gridDiv[0].addEventListener("mouseover", () => {
@@ -30,4 +39,4 @@ for (let i = 0; i < gridDivs.length; i++) {
 //CHANGE ELEMENT DISPLAY TO VISIBLE 
 
 
-});
+//});
